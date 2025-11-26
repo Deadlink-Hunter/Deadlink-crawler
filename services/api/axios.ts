d3fecc6e98305fac24e57e49853da70d28d589da
Tamir Abutbul <1tamir198@gmail.com>
2025-11-26 19:08:17 +0200

    Merge pull request #2 from Deadlink-Hunter/add-axios-service
    feat: introduce axios dependency and set up a centralized API client …

---
+++ services/api/axios.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosInstance } from 'axios';
+import { API_TIMEOUT } from '../../constants/api';
+
+const createAxiosInstance = (): AxiosInstance => {
+  const instance = axios.create({
+    timeout: API_TIMEOUT,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return instance;
+};
+
+export const apiClient = createAxiosInstance();
